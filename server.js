4641639b0d577d0c8c1ebc97faecb2b8d85c20d5
aaron <aaron_lai@hotmail.com>
2018-10-29 00:40:26 +0000

    Created server.js file

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict'
+//keeping their format for explaination if I need later
+// ================================================================
+// get all the tools we need
+// ================================================================
+var express = require('express');
+var routes = require('./routes/index.js');
+var port = process.env.PORT || 3000;
+
+var app = express();
+
+// ================================================================
+// setup our express application
+// ================================================================
+
+app.use('/public', express.static(process.cwd() + '/public'));
+app.set('view engine', 'ejs');
+
+// ================================================================
+// setup routes
+// ================================================================
+routes(app);
+
+// ================================================================
+// start our server
+// ================================================================
+app.list(port, function() {
+  console.log('server listening on port ' + port + '...');
+});
